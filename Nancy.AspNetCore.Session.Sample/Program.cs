0a873ba70d853979810655aef46b4266b6659026
Jeff Pang <jeff.tspang@gmail.com>
2017-08-12 03:43:25 +0800

    recreate sample

---
+++ Nancy.AspNetCore.Session.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nancy.AspNetCore.Session.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
